cda2d4313ac1892cdef815170839d4cf7f7c3e57
2305A21L51Pooja <2305A21L51@sru.edu.in>
2024-11-22 13:59:44 +0530

    Add files via upload

--- efficiency.py
+++ efficiency.py
@@ -5,7 +5,6 @@ def Gen_Efficiency(V,CL,K,IL,Rse,Ra):
     Eff = (K*V*IL)/(K*V*IL+CL+CUL)*100
     return CUL,Eff
 
-
 st.title("2305A21L51-PS10")
 st.subheader("Calculate the Efficency of DC Sereies Generator at Various Load")
 
@@ -14,6 +13,7 @@ st.subheader("Calculate the Efficency of DC Sereies Generator at Various Load")
 
 col1, col2 = st.columns(2)
 with col1:
+ st.subheader("Input Parameters:")
  with st.container(border=True):
     V = st.number_input("V:in Volts)", value=100)
     IL =st.number_input("IL:in Amps", value=10.00)
@@ -27,6 +27,7 @@ with col1:
 
 
 with col2:
+ st.subheader("Results:")
  with st.container(border=True):
     CUL,Eff = Gen_Efficiency(V,CL,IL,K,Rse,Ra)
     st.write(f"Copper losses = {CUL} W")
